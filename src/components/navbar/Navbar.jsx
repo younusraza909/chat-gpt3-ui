78e0891714e6489cf555457d6c25f1f9423606ba
younusraza909 <younusraza909@gmail.com>
2024-05-06 10:26:17 +0500

    navbar for desktop setup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import "./navbar.css"
+import React from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="Logo" />
         </div>
-    )
-}
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#whatis">What is GPT?</a>
+          </p>
+          <p>
+            <a href="#openAi">Open AI</a>
+          </p>
+          <p>
+            <a href="#case">Case Studies</a>
+          </p>
+          <p>
+            <a href="#library">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
 
-export default Navbar
+        <button type="button">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
