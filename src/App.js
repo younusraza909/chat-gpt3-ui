d90fa0620797ddea666dff9fc92cdb7c5b89d514
younusraza909 <younusraza909@gmail.com>
2024-05-13 11:01:14 +0500

    possiblty section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 import { Brand, Navbar } from "./components";
-import { Features, Header, WhatGPT3 } from "./containers";
+import { Features, Header, Possibility, WhatGPT3 } from "./containers";
 
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
       <Brand />
       <WhatGPT3 />
       <Features />
+      <Possibility />
     </div>
   );
 };
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import "./possibility.css"
+import React from "react";
+import "./possibility.css";
+
+import possiblity from "../../assets/possibility.png";
 
 const Possibility = () => {
-    return (
-        <div>
-            possibility
-        </div>
-    )
-}
+  return (
+    <div className="gpt3-possiblity section__padding">
+      <div className="gpt3-possiblity__image">
+        <img src={possiblity} alt="Possiblity" />
+      </div>
 
-export default Possibility
+      <div className="gpt3-possiblity__content">
+        <span>Request Early Access to Get Started</span>
+        <h1>
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <span>Request Early Access to Get Started</span>
+      </div>
+    </div>
+  );
+};
+
+export default Possibility;
